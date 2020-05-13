fc5c2c38c4112ba047b38066326e77c4eac3eebd
Tejas Upadhya <33410261+upadhyatejas@users.noreply.github.com>
2020-05-13 20:29:22 +0530

    added hooks

--- Basic_React_With_Props/src/App.js
+++ Basic_React_With_Props/src/App.js
@@ -1,15 +1,13 @@
 //react code in a separate class
 import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SeachParams";
 
 const App = () => {
   return (
     <div>
       <h1 id="something-important">Adopt-Me</h1>
-      <Pet name="luna" animal="dog" breed="havanese" age="11" />
-      <Pet name="cheddar" animal="dog" breed="shiba-inu" age="6" />
-      <Pet name="doink" animal="cat" breed="mixed" age="5" />
+      <SearchParams />
     </div>
   );
 };
