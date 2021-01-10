8cbca55de50555c11a8d3c1deda9e9944a89ff51
Tejas Upadhya <33410261+upadhyatejas@users.noreply.github.com>
2021-01-10 19:51:36 +0530

    Intro to React Final Commit
    Intro to React Final Commit

--- Adopt-Me/src/modal.js
+++ Adopt-Me/src/modal.js
@@ -1,21 +1,19 @@
-import React, { useEffect, useRef} from 'react';
-import { createPortal } from 'react-dom' ;
-
-const modalRoot = document.getElementById('modal')
+import React, { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
 
 const Modal = ({ children }) => {
-    const elRef = useRef(null);
-    if(!elRef.current){
-        const div = document.createElement('div');
-        elRef.current = div
-    }
-     useEffect(() =>{
-         const modalRoot = document.getElementById('modal');
-         modalRoot.appendChild(elRef.current);
+  const elRef = useRef(null);
+  if (!elRef.current) {
+    const div = document.createElement("div");
+    elRef.current = div;
+  }
+  useEffect(() => {
+    const modalRoot = document.getElementById("modal");
+    modalRoot.appendChild(elRef.current);
 
-         return () => modalRoot.removeChild(elRef.current);
-     }), [];
-     return createPortal(<div>{children}</div>, elRef.current);
+    return () => modalRoot.removeChild(elRef.current);
+  }, []);
+  return createPortal(<div>{children}</div>, elRef.current);
 };
 
 export default Modal;
